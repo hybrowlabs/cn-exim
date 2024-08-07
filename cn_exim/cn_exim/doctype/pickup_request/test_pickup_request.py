040fcfda873fdc1f97594d8aeecc7365d63cdb13
prathamjadhav15 <prathamjadhav052@gmail.com>
2024-08-07 17:22:46 +0530

    Doctype Created

---
+++ cn_exim/cn_exim/doctype/pickup_request/test_pickup_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Prathamesh Jadhav and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPickupRequest(FrappeTestCase):
+	pass
