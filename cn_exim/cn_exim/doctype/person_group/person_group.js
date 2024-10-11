ca77004012b02ac0d496af7a89cd1122de47450d
PIthiya-Hardik <pithiyahardik18@gmail.com>
2024-10-11 11:45:08 +0530

    rfq added fields for import

---
+++ cn_exim/cn_exim/doctype/person_group/person_group.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Prathamesh Jadhav and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Person Group", {
+// 	refresh(frm) {
+
+// 	},
+// });
