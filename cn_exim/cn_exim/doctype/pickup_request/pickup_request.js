1d8a7c2acfd0a9916871c13cbfc9aa982506ec24
PIthiya-Hardik <pithiyahardik18@gmail.com>
2024-10-23 18:11:47 +0530

    pickup request to create rfq and pre alert

--- cn_exim/cn_exim/doctype/pickup_request/pickup_request.js
+++ cn_exim/cn_exim/doctype/pickup_request/pickup_request.js
@@ -3,6 +3,35 @@
 
 frappe.ui.form.on("Pickup Request", {
     refresh(frm) {
+        if (frm.doc.docstatus == 1) {
+            frm.add_custom_button("Request for Quotation", function () {
+                frappe.new_doc('Request for Quotation', {
+                    'custom_type': "Logistics",
+                    'custom_pickup_request': frm.doc.name,
+                    'custom_port_of_loading': frm.doc.pol,
+                    'custom_mode_of_shipment': frm.doc.mode_of_shipment,
+                    'custom_pickup_supplier': frm.doc.name_of_supplier,
+                    'custom_shipment_type': frm.doc.type_of_shipments,
+                    'custom_no_of_pkg_units': frm.doc.no_of_boxes,
+                    'custom_port_of_destination': frm.doc.pod,
+                    'custom_vol_weight': frm.doc.gross_weight,
+                    'custom_actual_weights': frm.doc.chargeable_weight,
+                    'custom_remarks': frm.doc.remarks,
+                    'incoterm': frm.doc.incoterms,
+                }).then(doc => {
+                    frappe.set_route('Form', 'Request for Quotation', doc.name);
+                });
+            }, ("Create"))
+
+            frm.add_custom_button("Pre Alert", function () {
+                frappe.new_doc("Pre Alert", {
+                    'pickup_request':frm.doc.name
+                }).then(doc => {
+                    frappe.set_route('Form', 'Pre Alert', doc.name)
+                })
+            }, ("Create"))
+        }
+
         frm.set_query('supplier_address', function (doc) {
             return {
                 query: 'frappe.contacts.doctype.address.address.address_query',
@@ -82,37 +111,37 @@ frappe.ui.form.on("Pickup Request", {
         })
         frm.set_value("total_amount", total_amount)
     },
-    mode_of_shipment:function(frm){
-        if (frm.doc.mode_of_shipment == "Ocean liner"){
+    mode_of_shipment: function (frm) {
+        if (frm.doc.mode_of_shipment == "Ocean liner") {
             frm.set_value("type_wise_value", 6000)
         }
-        else if(frm.doc.mode_of_shipment == "MOS1-AIR"){
+        else if (frm.doc.mode_of_shipment == "MOS1-AIR") {
             frm.set_value("type_wise_value", 5000)
         }
     }
 })
 
 frappe.ui.form.on("Dimension Calculation", {
-    length:function(frm, cdt, cdn){
+    length: function (frm, cdt, cdn) {
         var row = locals[cdt][cdn]
         dimension_calculation(frm, row)
     },
-    width:function(frm, cdt, cdn){
+    width: function (frm, cdt, cdn) {
         var row = locals[cdt][cdn]
         dimension_calculation(frm, row)
     },
-    height:function(frm,cdt, cdn){
+    height: function (frm, cdt, cdn) {
         var row = locals[cdt][cdn]
         dimension_calculation(frm, row)
     }
 })
 
-function dimension_calculation(frm, row){
+function dimension_calculation(frm, row) {
     var weight = ((row.width ? row.width : 1) * (row.length ? row.length : 1) * (row.height ? row.height : 1)) / (frm.doc.type_wise_value ? frm.doc.type_wise_value : 1);
     row.weight = weight
     frm.refresh_field("dimension_calculation")
     var total_weight = 0
-    frm.doc.dimension_calculation.forEach(item =>{
+    frm.doc.dimension_calculation.forEach(item => {
         total_weight += row.weight
     })
     frm.set_value("chargeable_weight", total_weight)
