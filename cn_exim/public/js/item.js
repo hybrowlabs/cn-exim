1aafe1a65c29eafd93547014e613ed3676a97d47
PIthiya-Hardik <pithiyahardik18@gmail.com>
2024-11-07 13:21:38 +0530

    some field added in item mastre and material tyep doctype

---
+++ cn_exim/public/js/item.js
@@ -0,0 +1,26 @@
+frappe.ui.form.on("Item", {
+    custom_material_type: function (frm) {
+        if (frm.doc.custom_material_type != undefined) {
+            frappe.call({
+                method:"frappe.client.get_list",
+                args:{
+                    "doctype":"Material Type",
+                    filters:{
+                        'name':frm.doc.custom_material_type
+                    },
+                    fields:['account_refrence']
+                },
+                callback:function(r){
+                    var account_reference = r.message[0]['account_refrence']
+                    frm.set_query("valuation_classes", "custom_plant_details", function () {
+                        return {
+                            filters: {
+                                "account_category_refrence": account_reference
+                            }
+                        }
+                    })
+                }
+            })
+        }
+    }
+})
