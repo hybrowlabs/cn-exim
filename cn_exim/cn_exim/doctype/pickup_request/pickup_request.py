36900d0ba75cb6501a86d263872273869395d665
PIthiya-Hardik <pithiyahardik18@gmail.com>
2024-11-25 10:25:25 +0530

    some chnage in pre-alert-chack list

--- cn_exim/cn_exim/doctype/pickup_request/pickup_request.py
+++ cn_exim/cn_exim/doctype/pickup_request/pickup_request.py
@@ -6,16 +6,19 @@ from frappe.model.document import Document
 
 
 class PickupRequest(Document):
-	@frappe.whitelist()
-	def get_items(self,po):
-		self.purchase_order_details=[]
-		for i in po:
-			doc=frappe.get_doc("Purchase Order",i.get("po_number"))
-			for j in doc.items:
-				self.append("purchase_order_details",{
-					"item":j.item_code,
-					"material":j.item_name,
-					"quantity":j.qty,
-					"po_number":i.get("po_number"),
-					"material_desc":j.description
-				})
+    @frappe.whitelist()
+    def get_items(self, po):
+        self.purchase_order_details = []
+        for i in po:
+            doc = frappe.get_doc("Purchase Order", i.get("po_number"))
+            for j in doc.items:
+                self.append(
+                    "purchase_order_details",
+                    {
+                        "item": j.item_code,
+                        "material": j.item_name,
+                        "quantity": j.qty,
+                        "po_number": i.get("po_number"),
+                        "material_desc": j.description,
+                    },
+                )
